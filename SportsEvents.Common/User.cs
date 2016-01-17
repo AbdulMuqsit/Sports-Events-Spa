fe6df57d36dfb8cd5f1a6fd086325d0f05347bbe
AMuqsit <abdemuqsit@hotmail.com>
2016-01-17 17:22:17 +0500

    add untracked files

---
+++ SportsEvents.Common/User.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SportsEvents.Common
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<Event> RegisteredEvents { get; set; }
+
+
+
+
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+
+
+            
+        }
+    }
+}
---
+++ SportsEvents.EntityFramework/SportsEventsDbContext.cs
@@ -0,0 +1,28 @@
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SportsEvents.Common;
+
+namespace SportsEvents.EntityFramework
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
+    public class SportsEventsDbContext : IdentityDbContext<User>
+    {
+        public DbSet<Event> Events { get; set; }
+
+
+
+
+        public SportsEventsDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+
+
+        }
+
+        public static SportsEventsDbContext Create()
+        {
+            return new SportsEventsDbContext();
+        }
+    }
+}

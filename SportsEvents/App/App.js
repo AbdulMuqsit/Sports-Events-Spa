bad614be12b14f0f57f5ffee48d8b031aa0a7d1a
AMuqsit <abdemuqsit@hotmail.com>
2016-01-19 15:07:51 +0500

    add signup controller update eventscontroller add toastr

--- SportsEvents/App/App.js
+++ SportsEvents/App/App.js
@@ -1,3 +1,5 @@
-﻿var events = angular.module('events', []);
+﻿var customDirectives = angular.module('customDirectives', []);
+var events = angular.module('events', ['customDirectives']);
+var user = angular.module('user', []);
 
-var sportsEvents = angular.module('SportsEvents', ['events']);
+var sportsEvents = angular.module('SportsEvents', ['events', 'user','customDirectives']);
--- SportsEvents/App/Features/Events/AddEvent/AddEventController.js
+++ SportsEvents/App/Features/Events/AddEvent/AddEventController.js
@@ -1,21 +1,60 @@
-﻿events.controller('AddEventController', ['$http', function ($http) {
-    $scope.submit = function (model) {
-        var url = "/Api/Events";
-        var event = {
-            "Description": model.description,
-            "Details": model.details,
-            "BeginTime": model.beginTime,
-            "EndTime": model.endTime,
-            "BeginDate": model.beginDate,
-            "EndDate": model.endDate,
-            "CityId": model.cityId,
-            "CountryId": model.CountryId,
-            "SportId": model.sportId,
-            "SportTypeId": model.sportTypeId
+﻿events.controller("AddEventController", [
+    "$http", function($http) {
+        $scope.submit = function(model) {
+
+
+            function generateBase64String(url, callback, outputFormat) {
+                var img = new Image();
+                img.crossOrigin = "Anonymous";
+                img.onload = function() {
+                    var canvas = document.createElement("CANVAS");
+                    var ctx = canvas.getContext("2d");
+                    var dataURL;
+                    canvas.height = this.height;
+                    canvas.width = this.width;
+                    ctx.drawImage(this, 0, 0);
+                    dataURL = canvas.toDataURL(outputFormat);
+                    callback(dataURL);
+                    canvas = null;
+                };
+                img.src = url;
+            }
+
+            var url = "/Api/Events";
+            var event = {
+                "Description": model.description,
+                "Details": model.details,
+                "BeginTime": model.beginTime,
+                "EndTime": model.endTime,
+                "BeginDate": model.beginDate,
+                "EndDate": model.endDate,
+                "CityId": model.cityId,
+                "CountryId": model.CountryId,
+                "SportId": model.sportId,
+                "SportTypeId": model.sportTypeId,
+                "Zip": model.zip,
+                "StartingPrice": model.startingPrice,
+                "LineOne": model.lineOne,
+                "LineTwo": model.lineTwo,
+                "State": model.State,
+                "VideoLink": model.videoLink,
+                "ExternalLink": model.externalLink,
+                "IsFeatured": model.isFeatured
+
+
+            };
+            var picturesStirngs = [];
+            event.Icon = generateBase64String(model.icon);
+            for (var i = 0; i < model.pictures.length; i++) {
+                picturesStirngs[i] = generateBase64String(model.pictures[i]);
+            }
+
+            event.Pictures = picturesStirngs;
+            $http.post(url, user).then(function(data) {
+
+            }, function(data) {
+
+            });
         };
-
-        $http.post(url, model).then(function (data) {
-
-        });
     }
-}]);
+]);

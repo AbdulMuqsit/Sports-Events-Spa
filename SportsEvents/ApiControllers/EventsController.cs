93626fc2e7554e37a5f857ad48899820f3d789b9
Umarah Qaseem <umarahqaseem@hotmail.com>
2016-02-11 13:50:18 +0500

    Updated EventsController (AcceptRegistrationRequests)

--- SportsEvents/ApiControllers/EventsController.cs
+++ SportsEvents/ApiControllers/EventsController.cs
@@ -6,6 +6,8 @@ using SportsEvents.Infrastructure;
 using SportsEvents.Models;
 using Microsoft.AspNet.Identity;
 using System.Linq;
+using SportsEvents.Common.Entities;
+using System.Collections.Generic;
 
 namespace SportsEvents.ApiControllers
 {
@@ -53,6 +55,70 @@ namespace SportsEvents.ApiControllers
             {
                 return InternalServerError(ex);
             }
+        }
+
+        [Route("BookmarkEvents")]
+        public async Task<IHttpActionResult> BookmarkEvents(BookmarkEventsModel model)
+        {
+            var user = await UserManager.Users.Include(u => u.BookmarkedEvents).FirstOrDefaultAsync(u => u.UserName == User.Identity.Name);
+            var event_ = await DbContext.Events.FindAsync(model.id);
+            if (user.BookmarkedEvents == null)
+            {
+                user.BookmarkedEvents = new List<Event>();
+            }
+            user.BookmarkedEvents.Add(event_);
+            await UserManager.UpdateAsync(user);
+            return Ok(event_);
+        }
+
+        [Route("RegistrationRequests")]
+        public async Task<IHttpActionResult> RegisterationRequests(RegistrationRequestsModel model)
+        {
+            var user = await UserManager.Users.Include(u => u.RegistrationRequests).FirstOrDefaultAsync(u => u.UserName == User.Identity.Name);
+            var event_ = await DbContext.Events.FindAsync(model.id);
+            if (user.RegistrationRequests == null)
+            {
+                user.RegistrationRequests = new List<Event>();
+            }
+            user.BookmarkedEvents.Add(event_);
+            await UserManager.UpdateAsync(user);
+            return Ok(event_);
+        }
+
+        [Route("AcceptRegistrationRequests")]
+        public async Task<IHttpActionResult> AcceptRegistrationRequests(AcceptRegistrationRequestsModel model)
+        {
+            var user = await UserManager.Users.Include(u => u.RegisteredEvents).Include(u=>u.RegistrationRequests).Where(u=>u.UserName == model.userName).FirstOrDefaultAsync();
+            var event_ = await DbContext.Events.Include(ev => ev.RegisteredVisitors).Include(ev => ev.RegisterRequestVisitors).FirstOrDefaultAsync(ev=>ev.Id == model.eventId);
+            var organizer = await UserManager.Users.FirstOrDefaultAsync(u => u.UserName == User.Identity.Name);
+            var organizerEvents = DbContext.Events.Where(ev => ev.OrganizerName == User.Identity.Name).ToList();
+
+            if (!organizerEvents.Any(ev=>ev.Id == model.eventId))
+            {
+                return BadRequest();
+            }
+            if (!event_.RegisterRequestVisitors.Any(u=>u.UserName==model.userName))
+            {
+                return BadRequest();
+            }
+            if (!user.RegistrationRequests.Any(ev=>ev.Id==model.eventId))
+            {
+                return BadRequest();                
+            }
+            event_.RegisterRequestVisitors.Remove(user);
+            event_.RegisteredVisitors.Add(user);
+            user.RegistrationRequests.Remove(event_);
+            user.RegisteredEvents.Add(event_);
+
+            var dbEntityEntryEvent = DbContext.Entry(event_);
+            dbEntityEntryEvent.State = EntityState.Modified;
+
+            var dbEnttityEntryUser = DbContext.Entry(user);
+            dbEnttityEntryUser.State = EntityState.Modified;
+
+            await UserManager.UpdateAsync(user);
+            await DbContext.SaveChangesAsync();
+            return Ok();
         }
 
         public async Task<IHttpActionResult> Delete(int id)
